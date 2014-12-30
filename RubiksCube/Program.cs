b87bcc912e6250166eff0d8af7465da0f2f8441f
dfdemar <dfdemar@gmail.com>
2014-12-29 23:54:42 -0500

    adding rotation

--- RubiksCube/Program.cs
+++ RubiksCube/Program.cs
@@ -1,19 +1,39 @@
-﻿﻿using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 public class RubiksCube
 {
     private IDictionary<Face, CubeFace> Faces { get; set; }
     public enum Face { F, B, L, R, U, D }
+    public enum FaceType { Side, Top, Bottom }
 
     public RubiksCube()
     {
         Faces = new Dictionary<Face, CubeFace>();
-        Faces.Add(Face.U, new CubeFace(new[] { Face.R, Face.B, Face.L, Face.F }));
-        Faces.Add(Face.L, new CubeFace(new[] { Face.F, Face.U, Face.B, Face.D }));
-        Faces.Add(Face.F, new CubeFace(new[] { Face.R, Face.U, Face.L, Face.D }));
-        Faces.Add(Face.R, new CubeFace(new[] { Face.B, Face.U, Face.F, Face.D }));
-        Faces.Add(Face.D, new CubeFace(new[] { Face.R, Face.F, Face.L, Face.B }));
-        Faces.Add(Face.B, new CubeFace(new[] { Face.R, Face.D, Face.L, Face.U }));
+        Faces.Add(Face.F, new CubeFace(FaceType.Side, new[] { Face.U, Face.R, Face.D, Face.L }));
+        Faces.Add(Face.L, new CubeFace(FaceType.Side, new[] { Face.U, Face.F, Face.D, Face.B }));
+        Faces.Add(Face.R, new CubeFace(FaceType.Side, new[] { Face.U, Face.B, Face.D, Face.F }));
+        Faces.Add(Face.B, new CubeFace(FaceType.Side, new[] { Face.U, Face.L, Face.B, Face.R }));
+        Faces.Add(Face.U, new CubeFace(FaceType.Top, new[] { Face.B, Face.R, Face.F, Face.L }));
+        Faces.Add(Face.D, new CubeFace(FaceType.Bottom, new[] { Face.B, Face.R, Face.F, Face.L }));
+    }
+
+    public void Rotate(CubeFace face)
+    {
+        if (face.FaceType == FaceType.Side)
+        {
+            int n = face.Values.GetLength(0);
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = i; j < n - i - 1; j++)
+                {
+                    tmp=a[i][j];
+                    a[i][j]=a[j][n-i-1];
+                    a[j][n-i-1]=a[n-i-1][n-j-1];
+                    a[n-i-1][n-j-1]=a[n-j-1][i];
+                    a[n-j-1][i]=tmp;
+                }
+            }
+        }
     }
 }
 
@@ -21,10 +41,12 @@ public class CubeFace
 {
     public int[,] Values { get; set; }
     public IEnumerable<char> Connections { get; set; }
+    public RubiksCube.FaceType FaceType { get; set; }
 
-    public CubeFace(IEnumerable<RubiksCube.Face> connections)
+    public CubeFace(RubiksCube.FaceType faceType, IEnumerable<RubiksCube.Face> connections)
     {
-        Values = new int[3, 3];
+        FaceType = faceType;
+        Values = new int[3,3];
     }
 }
 
